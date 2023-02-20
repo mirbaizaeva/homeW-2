0b01d5cbf4daced2420c6a520c7a1772b60c3247
Nurjamal <n.mirbaizaeva@gmail.com>
2023-02-20 22:04:11 +0600

    Initial Commit

---
+++ homeW-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeW-2
+//
+//  Created by Nurjamal Mirbaizaeva on 20/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
